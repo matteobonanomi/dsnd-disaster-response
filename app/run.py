d1bc0008730bf71207d56a44762dc833c14c66cb
matteobonanomi <mat.bon92@gmail.com>
2018-12-23 19:30:51 +0100

    [FIX] Minor Fixes to app 1) Add missing nltk packages 2) Add missing customized estimators

--- app/run.py
+++ app/run.py
@@ -3,7 +3,10 @@ import plotly
 import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk import pos_tag, word_tokenize
+import nltk
+
 from sklearn.base import BaseEstimator, TransformerMixin
 
 from flask import Flask
@@ -15,6 +18,23 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 def tokenize(text):
     tokens = word_tokenize(text)
